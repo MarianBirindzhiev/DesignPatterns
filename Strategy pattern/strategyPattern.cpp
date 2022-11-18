6aee4f585876df3820a6b0948744a61243c6b172
Marian Birindzhiev <marianbirindzhiev@gmail.com>
2022-11-18 11:55:24 +0200

    Pushing

--- Strategy pattern/strategyPattern.cpp
+++ Strategy pattern/strategyPattern.cpp
@@ -3,9 +3,65 @@
 #include <iostream>
 #include <algorithm>
 
+class IBehaviorFly
+{
+public:
+    IBehaviorFly(){};
+    virtual ~IBehaviorFly(){};
+    
+    virtual void fly() = 0;
+};
+
+class IBehaviorQuach
+{
+public:
+    IBehaviorQuach(){};
+    virtual ~IBehaviorQuach(){};
+
+    virtual void quach() = 0;
+};
+
+class Duck
+{
+private:
+    IBehaviorFly* m_fb;
+    IBehaviorQuach* m_qb;
+public:
+    Duck( IBehaviorFly* fb, IBehaviorQuach* qb)
+    {
+        m_fb=fb;
+        m_qb=qb;
+    }
+    ~Duck(){};
+};
+
+class simpleQuach : public IBehaviorQuach
+{
+public:
+    void quach() override
+    {
+        std::cout<<"Quach";
+    }
+};
+class noFlying : public IBehaviorFly
+{
+public:
+    void fly() override
+    {
+        std::cout<<"This type of duck can't fly";
+    }
+};
+class yesFlying : public IBehaviorFly
+{
+public:
+    void fly() override
+    {
+        std::cout<<"This type of duck can fly";
+    }
+};
 
 
 int main()
 {
-
+    
 }
